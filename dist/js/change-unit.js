1038c7501479213e7b852287b77eb5b678821b24
cateruu <cater981pl@gmail.com>
2022-05-25 16:55:21 +0200

    Layout

---
+++ dist/js/change-unit.js
@@ -0,0 +1,10 @@
+const changeUnitBtn = document.querySelector('.change-unit');
+const background = document.querySelector('.change-unit__background');
+
+changeUnitBtn.addEventListener('click', () => {
+  let activeUnit = document.querySelector('.active');
+  
+  if (activeUnit.dataset.unit == 'celsius') {
+    
+  }
+});
