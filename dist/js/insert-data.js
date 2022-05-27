30c146ceaac7715b0848e158346a2dc27ca50436
cateruu <cater981pl@gmail.com>
2022-05-27 15:09:15 +0200

    Air Quality API
    Data insertion and indicator color change

--- dist/js/insert-data.js
+++ dist/js/insert-data.js
@@ -10,6 +10,15 @@ const windDirectionElem = document.querySelector('.wind-direction');
 const humidityElem = document.querySelector('.humidity');
 const lastUpdatedElem = document.querySelector('.last-updated span');
 
+// Air Quality elements
+const carbonElem = document.querySelector('.carbon');
+const ozoneElem = document.querySelector('.ozone');
+const nitrogenElem = document.querySelector('.nitrogen');
+const sulphurElem = document.querySelector('.sulphur');
+const pm2_5Elem = document.querySelector('.pm2-5');
+const pm10Elem = document.querySelector('.pm10');
+const indicatorElem = document.querySelector('.air-quality__indicator');
+
 export function insertData(data) {
   const { current: { temp_c: temp, humidity, pressure_mb: pressure, wind_dir: direction, last_updated, condition: { text, icon },
           air_quality: { co, o3, no2, so2, pm2_5, pm10 } }, 
@@ -20,14 +29,45 @@ export function insertData(data) {
   cityElem.innerText = name;
   countryElem.innerText = country;
 
-  degreesElem.innerHTML = temp + '&#8451';
+  degreesElem.innerHTML = temp + ' C';
   weatherDescriptionElem.innerText = text;
 
   pressureElem.innerText = pressure + ' hPa';
   windDirectionElem.innerText = direction;
   humidityElem.innerText = humidity + '%';
 
-console.log(data.current.air_quality['us-epa-index']);
+  // Air Quality
+  carbonElem.innerText = ` ${Math.round(co * 100) / 100} (μg/m3)`;
+  ozoneElem.innerText = ` ${Math.round(o3 * 100) / 100}`;
+  nitrogenElem.innerText = ` ${Math.round(no2 * 100) / 100}`;
+  sulphurElem.innerText = ` ${Math.round(so2 * 100) / 100}`;
+  pm2_5Elem.innerText = ` ${Math.round(pm2_5 * 100) / 100}`;
+  pm10Elem.innerText = ` ${Math.round(pm10 * 100) / 100}`; 
+
+  // Air Quality indicator
+  switch ((data.current.air_quality['us-epa-index'])) {
+    case 1:
+      indicatorElem.style.backgroundColor = '#23e233';
+      break;
+    case 2:
+      indicatorElem.style.backgroundColor = '#f1f520';
+      break;
+    case 3:
+      indicatorElem.style.backgroundColor = '#f5b220';
+      break;
+    case 4:
+      indicatorElem.style.backgroundColor = '#f13a22';
+      break;
+    case 5:
+      indicatorElem.style.backgroundColor = '#9e22f1';
+      break;
+    case 6:
+      indicatorElem.style.backgroundColor = '#800000';
+      break;
+    default:
+      indicatorElem.style.backgroundColor = '#50ec30';
+      break;
+  }
 
   lastUpdatedElem.innerText = last_updated;
 }
